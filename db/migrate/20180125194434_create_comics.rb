362edbdbf1e16b727c3a36cb09cc56c35afe253a
Timothy Beck <socaltdb@gmail.com>
2018-01-25 11:52:07 -0800

    fix migration file

--- db/migrate/20180125194434_create_comics.rb
+++ db/migrate/20180125194434_create_comics.rb
@@ -1,8 +1,8 @@
 class CreateComics < ActiveRecord::Migration[5.1]
   def change
     create_table :comics do |t|
-      t.title :string
-      t.alt :string
+      t.string :title
+      t.string :alt
 
       t.timestamps
     end
